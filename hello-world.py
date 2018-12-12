247be8ed489ff5e5d4ba22829ae5ded5017bcac5
Hongxia <cngaohongxia@gmail.com>
2018-12-12 22:41:21 +0900

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671
+{\fonttbl}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+{\info
+{\author Hongxia GAO}
+{\*\company University of Tokyo}}\paperw11900\paperh16840\margl1440\margr1440\vieww16200\viewh13200\viewkind0
+}
